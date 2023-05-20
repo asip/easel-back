b641b9681089450367b681fe8eab544c63d910ee
Yasumasa Ashida <ys.ashida@gmail.com>
2023-05-20 18:58:30 +0900

    refactor:  edit users table definition

--- db/migrate/20230520070104_change_users_name_email_index.rb
+++ db/migrate/20230520070104_change_users_name_email_index.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# ChangeUsersNameEmailIndex
 class ChangeUsersNameEmailIndex < ActiveRecord::Migration[7.0]
   def change
     change_table :users, bulk: true do |t|
