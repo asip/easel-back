57f7980ded83a4768b705e706000230ee87941ef
Yasumasa Ashida <ys.ashida@gmail.com>
2023-05-21 20:07:03 +0900

    chore: introduce ESLint

--- app/javascript/controllers/toast_controller.ts
+++ app/javascript/controllers/toast_controller.ts
@@ -4,12 +4,12 @@ import Toastify from 'toastify-js'
 export default class ToastController extends ApplicationController {
   static values = { messages: String };
 
-  messagesValue: String
+  messagesValue: string
 
   connect() {
     const messages = JSON.parse(this.messagesValue.valueOf())["messages"].reverse()
     //console.log(messages)
-    messages.forEach(message => {
+    messages.forEach((message: string) => {
       Toastify({
         text: message,
         duration: 2000
