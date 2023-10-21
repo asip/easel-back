2d89d509d6cae85e92830202240af45d2191ba53
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-21 17:04:23 +0900

    test(spec): update rails_helper & spec_helper

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include include FactoryBot::Syntax::Methods
+end
---
+++ spec/support/view_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include ViewComponent::TestHelpers, type: :component
+end
