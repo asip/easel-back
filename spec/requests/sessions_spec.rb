f0b4e3ed78a5dcce7788795f3aed351e91071894
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-22 19:00:27 +0900

    test(spec/request): edit request spec for api/v1/sessions_controller

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -8,20 +8,20 @@ describe 'Sessions', type: :request do
     let!(:user) { create(:user, password: 'testtest', password_confirmation: 'testtest') }
     let(:endpoint) { '/api/v1/sessions' }
 
-    context 'login' do
-      it 'success' do
+    context 'login (ログイン)' do
+      it 'success (成功)' do
         post endpoint,
              params: { user: { email: user.email, password: 'testtest' } },
              headers: { 'HTTP_ACCEPT_LANGUAGE' => 'jp' }
+        expect(response.status).to eq(200)
         json_data = json[:data]
         expect(json_data).to have_type('user')
         expect(json_data).to have_attribute('name')
         expect(json_data).to have_attribute('email')
-        expect(response.status).to eq(200)
       end
 
-      context 'failure' do
-        it 'invalid email' do
+      context 'failure (失敗)' do
+        it 'invalid email (未登録のEメールアドレスの場合)' do
           post endpoint,
                params: { user: { email: 'invalid@test.jp', password: 'testtest' } },
                headers: { 'HTTP_ACCEPT_LANGUAGE' => 'jp' }
@@ -30,7 +30,7 @@ describe 'Sessions', type: :request do
           expect(json_data[:messages]).to be_present
         end
 
-        it 'invalid password' do
+        it 'invalid password (パスワードが正しくない場合)' do
           post endpoint,
                params: { user: { email: user.email, password: 'invalidtest' } },
                headers: { 'HTTP_ACCEPT_LANGUAGE' => 'jp' }
@@ -39,6 +39,37 @@ describe 'Sessions', type: :request do
           expect(json_data[:messages]).to be_present
         end
       end
+    end
+  end
+
+  describe 'GET /api/v1/profile' do
+    let(:endpoint) { '/api/v1/profile' }
+
+    before do
+      @user = create(:user, password: 'testtest', password_confirmation: 'testtest')
+      @user.assign_token(User.issue_token(id: @user.id, email: @user.email))
+    end
+
+    context 'authenticate token (トークン認証)' do
+      it 'success (成功)' do
+        get endpoint, headers: {
+          'HTTP_ACCEPT_LANGUAGE' => 'jp',
+          'Authorization': "Bearer #{@user.token}"
+        }
+        expect(response.status).to eq(200)
+        json_data = json[:data]
+        expect(json_data).to have_type('user')
+        expect(json_data).to have_attribute('name')
+        expect(json_data).to have_attribute('email')
+      end
+    end
+
+    it 'failure (失敗)' do
+      get endpoint, headers: {
+        'HTTP_ACCEPT_LANGUAGE' => 'jp',
+        'Authorization': 'Bearer '
+      }
+      expect(response.status).to eq(401)
     end
   end
 end
