637526c9fc7d61f51f379f86963bf2b810474953
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-23 18:24:48 +0900

    test(spec/request): edit request spec for api/v1/frames_controller

--- spec/requests/frames_spec.rb
+++ spec/requests/frames_spec.rb
@@ -294,5 +294,39 @@ describe 'Frames', type: :request do
       end
     end
   end
+
+  describe 'DELETE /api/v1/frames/:id' do
+    let(:endpoint) { "/api/v1/frames/#{frame.id}" }
+    let(:endpoint_failure) { '/api/v1/frames/404' }
+    let!(:user) { create(:user, password: 'testtest', password_confirmation: 'testtest') }
+    let!(:frame) { create(:frame, :skip_validate, user_id: user.id) }
+
+    before do
+      user.assign_token(User.issue_token(id: user.id, email: user.email))
+    end
+
+    context 'get frame (フレーム情報削除)' do
+      it 'success (成功)' do
+        delete endpoint,
+               headers: {
+                 'HTTP_ACCEPT_LANGUAGE': 'jp',
+                 'Authorization': "Bearer #{user.token}"
+               }
+        expect(response.status).to eq 200
+        json_data = json[:data]
+        expect(json_data).to have_type('frame')
+        expect(json_data).to have_attribute('name')
+      end
+
+      it 'failure (失敗)' do
+        delete endpoint_failure,
+               headers: {
+                 'HTTP_ACCEPT_LANGUAGE': 'jp',
+                 'Authorization': "Bearer #{user.token}"
+               }
+        expect(response.status).to eq 404
+      end
+    end
+  end
 end
 # rubocop:enable Metrics/BlockLength
