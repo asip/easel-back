d79f2aeba11a369f3da7cc6a391c5f56474e0152
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-01 19:28:15 +0900

    refactor(model): edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,11 +64,10 @@ class User < ApplicationRecord
   with_options on: :with_validation do |save|
     save.validates :password, length: { minimum: 3 }, confirmation: true,
                               if: -> { new_record? || changes[:crypted_password] }
-    save.validates :password_confirmation, presence: true,
-                                           if: -> { new_record? || changes[:crypted_password] }
+    save.validates :password_confirmation, if: -> { new_record? || changes[:crypted_password] }
 
-    save.validates :name, presence: true, length: { in: 3..40 } # , format: { with: VALID_NAME_REGEX }
-    save.validates :email, presence: true, uniqueness: true, length: { max: 319 }, format: { with: VALID_EMAIL_REGEX }
+    save.validates :name, length: { minimun: 3, maximum: 40 } # , format: { with: VALID_NAME_REGEX }
+    save.validates :email, length: { minimum: 3, maximum: 319 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   end
 
   with_options on: :login do
