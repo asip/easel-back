e9bcbba6404a6f4d1deb0ff82316a6d72274672a
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-11 11:06:43 +0900

    refactor(controller): edit api/v1/oauth/sessions_controller

--- app/controllers/api/v1/oauth/sessions_controller.rb
+++ app/controllers/api/v1/oauth/sessions_controller.rb
@@ -16,22 +16,23 @@ module Api
         def create
           provider = auth_params[:provider]
 
-          login_from_oauth(provider)
-          cookies.permanent[:access_token] = @user.token
-          render json: AccountSerializer.new(@user).serializable_hash
+          user = login_from_oauth(provider)
+          cookies.permanent[:access_token] = user.token
+          render json: AccountSerializer.new(user).serializable_hash
         end
 
         private
 
         def login_from_oauth(provider)
-          if (@user = login_from(provider))
-            @user.assign_token(user_class.issue_token(id: @user.id, email: @user.email))
+          if (user = login_from(provider))
+            user.assign_token(user_class.issue_token(id: user.id, email: user.email))
           else
-            @user = create_from(provider)
-            @user.assign_token(user_class.issue_token(id: @user.id, email: @user.email))
+            user = create_from(provider)
+            user.assign_token(user_class.issue_token(id: user.id, email: user.email))
             reset_session
-            auto_login(@user)
+            auto_login(user)
           end
+          user
         end
 
         def auth_params
