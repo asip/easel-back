003c9a7b1fc17416b599f577d3812e4468785adc
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-17 15:38:00 +0900

    refactor(component): refactoring of toast component

--- app/components/toast/component.rb
+++ app/components/toast/component.rb
@@ -4,10 +4,22 @@
 module Toast
   # Component
   class Component < ViewComponent::Base
-    def initialize(flashes:)
+    def initialize(flash: {}, flashes: {})
       super
+      @flash = flash
       @flashes = flashes
+      @flash_json = flash_to_json
       @flashes_json = flashes.to_json
+    end
+
+    private
+
+    def flash_to_json
+      flash_ = {}
+      @flash.each do |key, value|
+        flash_[key] = [value]
+      end
+      flash_.to_json
     end
   end
 end
