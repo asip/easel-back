8548a6ba27a86790af05a0c143c0424a668b4335
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-09 15:43:31 +0900

    chore: edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,42 +3,42 @@ module.exports = {
     es2022: true
   },
   extends: [
-    "eslint:recommended",
-    "prettier"
+    'eslint:recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module"
+    sourceType: 'module'
   },
   rules: {
     // 共通のルールを設定
-    semi: ["error", "never"],
-    quotes: ["error", "single"],
-    indent: ["error", 2]
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2]
   },
   overrides: [
     {
-      files: ["**/*.ts"],
+      files: ['**/*.ts'],
       extends: [
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended'
       ],
-      parser: "@typescript-eslint/parser",
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        sourceType: "module",
-        project: "./tsconfig.json"
+        sourceType: 'module',
+        project: './tsconfig.json'
       },
       plugins: [
-        "@typescript-eslint"
+        '@typescript-eslint'
       ],
       rules: {
-        "@typescript-eslint/no-explicit-any": 1,
-        "@typescript-eslint/no-non-null-assertion": 1
+        '@typescript-eslint/no-explicit-any': 1,
+        '@typescript-eslint/no-non-null-assertion': 1
       }
     }
   ],
   ignorePatterns: [
-    "./app/assets/builds/**/*" // Ignore built files.
+    './app/assets/builds/**/*' // Ignore built files.
   ],
   root: true
 }
