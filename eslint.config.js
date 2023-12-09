4e4a652d7d461b24a65c9a6335d3fc77af79d0c8
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-09 19:01:44 +0900

    chore: edit eslint config

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import typescriptESLintParser from '@typescript-eslint/parser'
+import typescriptESLint from '@typescript-eslint/eslint-plugin'
+
+const compat = new FlatCompat()
+
+export default [
+  {
+    ignores: [
+      './app/assets/builds/**/*' // Ignore built files.
+    ]
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        es2022: true
+      }
+    },
+    rules: {
+      // 共通のルールを設定
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2]
+    }
+  },
+  // ,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module'
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: typescriptESLintParser,
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescriptESLint
+    },
+    rules: {
+      ...typescriptESLint.configs.recommended.rules,
+      ...typescriptESLint.configs["recommended-type-checked"].rules,
+      ...typescriptESLint.configs.strict.rules,
+      ...typescriptESLint.configs["strict-type-checked"].rules,
+      ...typescriptESLint.configs.stylistic.rules,
+      '@typescript-eslint/no-explicit-any': 1,
+      '@typescript-eslint/no-non-null-assertion': 1
+    }
+  }
+]
