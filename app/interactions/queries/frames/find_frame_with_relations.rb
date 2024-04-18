4f9d1f242f3f506b490c51a833f0915be0caaa2c
Yasumasa Ashida <ys.ashida@gmail.com>
2024-04-18 12:03:29 +0900

    style(interaction): edit find_frame_with_relations

--- app/interactions/queries/frames/find_frame_with_relations.rb
+++ app/interactions/queries/frames/find_frame_with_relations.rb
@@ -20,7 +20,12 @@ module Queries
         elsif @user.nil? && !@private.nil?
           Frame.eager_load(:user, comments: :user).find_by!(id: @frame_id, private: @private)
         elsif !@user.nil? && @private.nil?
-          Frame.eager_load(:user, comments: :user).merge(Frame.where(user_id: @user.id).or(Frame.where(private: false).where.not(user_id: @user.id))).find_by!(id: @frame_id)
+          Frame.eager_load(:user, comments: :user)
+          .merge(
+            Frame.where(user_id: @user.id).or(
+              Frame.where(private: false).where.not(user_id: @user.id)
+            )
+          ).find_by!(id: @frame_id)
         end
       end
     end
