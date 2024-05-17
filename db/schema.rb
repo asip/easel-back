9efbc3ef9f8abfbafc3f9540a94461a84f2e77eb
Yasumasa Ashida <ys.ashida@gmail.com>
2024-05-17 20:40:35 +0900

    refactor(model): edit user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_132713) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_112513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,7 +115,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_132713) do
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
-    t.index ["name", "email"], name: "index_users_on_name_and_email"
     t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token"
   end
