721e4f56fcb1c7267f59d47a3f19cf2322d36ff8
Yasumasa Ashida <ys.ashida@gmail.com>
2025-05-23 21:23:54 +0900

    fix(config): Fix CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ "localhost", Settings.origin, Settings.frontend.origin ]
+    origins [ Settings.reverse_proxy.origin, Settings.origin, Settings.frontend.origin ]
 
     resource "*",
              headers: :any,
