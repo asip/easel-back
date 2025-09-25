7dac71c00b62f6efa72a904cfc1dce0303795180
Yasumasa Ashida <ys.ashida@gmail.com>
2025-09-25 19:29:58 +0900

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import RubyPlugin from 'vite-plugin-ruby'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: 3036,
+    hmr: {
+      protocol: 'ws',
+    }
+  },
   plugins: [
     RubyPlugin(),
     tailwindcss()
