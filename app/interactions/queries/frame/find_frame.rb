125ed9957e1d53ac2ed81c3ceaeb714ade5db6b1
Yasumasa Ashida <ys.ashida@gmail.com>
2026-02-14 16:09:30 +0900

    refactor(interaction): Refactor queries/frame/find_frame(_with_relations)

--- app/interactions/queries/frame/find_frame.rb
+++ app/interactions/queries/frame/find_frame.rb
@@ -4,22 +4,22 @@
 class Queries::Frame::FindFrame
   include Query
 
-  def initialize(frame_id:, private: nil, user: nil)
+  def initialize(frame_id:, private: true, user: nil)
     @frame_id = frame_id
     @private = private
     @user = user
   end
 
   def execute
-    private_nil = @private.nil?
+    private_present = @private.present?
     scope = Frame
     if @user.blank?
-      if private_nil
+      if private_present
         scope.find_by!(id: @frame_id)
       else
         scope.find_by!(id: @frame_id, private: @private)
       end
-    elsif private_nil
+    elsif private_present
       user_id = @user.id
       scope.eager_load(:user, comments: :user)
            .merge(
