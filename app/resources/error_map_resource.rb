1e9391e4a2aac931df4f28002dcbe0d14d56df29
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-03 19:23:45 +0900

    refactor(controller/concern): Refactor api/error_renderable (#46)

--- app/resources/error_map_resource.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# ErrorMap Resource
-class ErrorMapResource < ApplicationResource
-  attributes :errors
-end
---
+++ app/resources/error_messages_resource.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# ErrorMessages Resource
+class ErrorMessagesResource < ApplicationResource
+  attributes :errors
+end
