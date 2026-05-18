2ba5b4a4f5ba20bd87ba05626018651e4e807b55
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-18 21:38:33 +0900

    refactor(controller/concern): Refactor time_zone/detect

---
+++ app/controllers/concerns/time_zone/detect.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# TimeZone::Detect module
+module TimeZone::Detect
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_time_zone
+  end
+
+  protected
+
+  def set_time_zone
+    # puts request.headers
+    @time_zone = request.headers["Time-Zone"]
+  end
+
+  def time_zone
+    @time_zone
+  end
+end
